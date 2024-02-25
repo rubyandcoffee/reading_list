32e726f413c9542414514d346d45ca0fb0a507d3
Alexandra Wolfe <rubyandcoffee@gmail.com>
2024-02-25 17:39:05 +0000

    Calculate amount of time required to read book

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -27,6 +27,13 @@ module BooksHelper
     series.books.map(&:current_state).all?('read')
   end
 
+  def time_to_read(book)
+    return unless book.total_pages.present?
+
+    in_minutes = book.total_pages * 1.5
+    in_hours = (in_minutes / 60).round
+  end
+
   private
 
   def state_klasses
