2e1fb75c258e5ef0e995018c80f59cf36c67c772
Alexandra Wolfe <rubyandcoffee@gmail.com>
2024-06-25 00:24:41 +0100

    Hide nested field link after clicking it

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,4 +9,17 @@ import "@nathanvda/cocoon"
 
 $(".new-goal").on('click', function() {
   $('.new-goal').text('Add another month');
+  $('.new-goal').addClass('hidden');
+});
+
+$('.rentals').on('cocoon:after-insert', function() {
+  $('.max-one-association').addClass('hidden');
+});
+
+$('.rentals').on('cocoon:after-remove', function() {
+  $('.max-one-association').removeClass('hidden');
+});
+
+$('.book_goals').on('cocoon:after-remove', function() {
+  $('.new-goal').removeClass('hidden');
 });
