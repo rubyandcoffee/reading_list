ec8d314572a85bea5b8a66fc74c1e00e125d1cca
Alexandra Wolfe <rubyandcoffee@gmail.com>
2024-09-07 13:56:34 +0100

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'rentals/destroy'
-  get 'books/yearly_goals', to: 'books#yearly_goals'
-  get 'books/reviews', to: 'books#reviews'
-  get 'books/buy', to: 'books#buy'
-  get 'books/generator', to: 'books#generator'
-  get 'books/export', to: 'books#export'
-  get 'books/unrated', to: 'books#unrated'
-
   resources :genres
 
   resources :authors do
@@ -17,18 +9,26 @@ Rails.application.routes.draw do
 
   resources :books do
     resources :rentals, only: :destroy
+
     collection do
-      post :import
+      post 'import'
+      get 'export'
+      get 'yearly_goals'
+      get 'reviews'
+      get 'buy'
+      get 'generator'
+      get 'unrated'
     end
+
     member do
       patch 'update_rating'
-      post 'remove_from_shelf/:id', to: 'books#remove_from_shelf', as: 'remove_from_shelf'
+      post 'remove_from_shelf'
     end
   end
 
   resources :series do
     collection do
-      post :import
+      post 'import'
     end
   end
 
