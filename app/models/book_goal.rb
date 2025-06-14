e92ad964d984ca02d553efc110c88f5ea640b799
Alexandra Wolfe <rubyandcoffee@gmail.com>
2025-06-14 10:11:28 +0100

    Add timeline relationship to book goal

--- app/models/book_goal.rb
+++ app/models/book_goal.rb
@@ -1,4 +1,5 @@
 class BookGoal < ApplicationRecord
   belongs_to :book
+  has_one :timeline, class_name: 'BookGoalTimeline', dependent: :destroy
   scope :this_year, -> { where(year: DateTime.now.year) }
 end
